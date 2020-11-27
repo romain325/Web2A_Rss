8fe2652faa1acb89e1c6830dafd8f7da623b445e
aulaborie <augustin.laborie@etu.uca.fr>
2020-11-27 14:15:54 +0100

    modif de la BD

--- DB/projetrss.sql
+++ DB/projetrss.sql
@@ -103,7 +103,7 @@ CREATE TABLE `source` (
   `id` int NOT NULL,
   `nom` varchar(30) NOT NULL,
   `lien` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 --
 -- Déchargement des données de la table `source`
